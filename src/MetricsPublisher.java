9487bd5ec2ef7e2b623ade6f115ec8eea24bb68c
VSut <vaino.sutinen@gmail.com>
2024-08-03 23:25:18 -0500

    followed README

--- src/MetricsPublisher.java
+++ src/MetricsPublisher.java
@@ -1,6 +1,8 @@
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.MetricDatum;
 import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
 import com.amazonaws.services.cloudwatch.model.StandardUnit;
 
@@ -35,6 +37,17 @@ public class MetricsPublisher {
                                                        final StandardUnit unit) {
 
         // TODO: implement
-        return null;
+        Dimension dimension = new Dimension()
+                .withName("ENVIRONMENT")
+                .withValue("PRODUCTION");
+        MetricDatum datum = new MetricDatum()
+                .withValue(value)
+                .withMetricName(metricName)
+                .withUnit(unit)
+                .withDimensions(dimension);
+        PutMetricDataRequest request = new PutMetricDataRequest()
+                .withNamespace("EXAMPLE/ORDERS")
+                .withMetricData(datum);
+        return request;
     }
 }
